f3c8c5e4f2ee5cd7ab3122b136d35a63eb2c26ec
Lindsey Maddox <lindseymaddox@Lindseys-iMac.local>
2016-04-17 08:13:51 -0500

    icon set

--- _scripts/main.js
+++ _scripts/main.js
@@ -13,36 +13,29 @@ function showNav(){
 	});
 }
 
-function placeholderSupported() {
-    test = document.createElement('input');
-    return ('placeholder' in test);
-}
-
 function initPlaceholders() {
 	if ($('input[placeholder]').length > 0) {
-		if (!placeholderSupported()) {
-			$('input[placeholder]').focus(function() {
+		$('input[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() === input.attr('placeholder')) {
+				input.val('');
+				input.removeClass('placeholder');
+			}
+		}).blur(function() {
+			var input = $(this);
+			if (input.val() === '' || input.val() === input.attr('placeholder')) {
+				input.addClass('placeholder');
+				input.val(input.attr('placeholder'));
+			}
+		}).blur();
+		$('input[placeholder]').parents('form').submit(function() {
+			$(this).find('[placeholder]').each(function() {
 				var input = $(this);
 				if (input.val() === input.attr('placeholder')) {
 					input.val('');
-					input.removeClass('placeholder');
 				}
-			}).blur(function() {
-				var input = $(this);
-				if (input.val() === '' || input.val() === input.attr('placeholder')) {
-					input.addClass('placeholder');
-					input.val(input.attr('placeholder'));
-				}
-			}).blur();
-			$('input[placeholder]').parents('form').submit(function() {
-				$(this).find('[placeholder]').each(function() {
-					var input = $(this);
-					if (input.val() === input.attr('placeholder')) {
-						input.val('');
-					}
-				});
 			});
-		}
+		});
 	}
 }
 
@@ -50,7 +43,6 @@ function firstLoad() {
 	initVars();
 	showNav();
 	initPlaceholders();
-	placeholderSupported();
 }
 
 $(function() {
